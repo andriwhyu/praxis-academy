5fa5362178380bb2f192c26e73410873e3668ae3
andriwhyu <andriruslam@gmail.com>
2020-01-17 10:53:31 +0700

    case without markdown

---
+++ novice/02-02/latihan/stf_widget_tapbox_b/android/app/src/main/kotlin/com/example/stf_widget_tapbox_b/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stf_widget_tapbox_b
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
